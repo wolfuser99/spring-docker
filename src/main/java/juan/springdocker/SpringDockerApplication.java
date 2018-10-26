c35ce6cf52eda2a4614a96da084b50f02e6cf7d8
Juan Antonio Cid <jucid@alumnos.ubiobio.cl>
2018-10-26 00:41:05 -0300

    initial commit

---
+++ src/main/java/juan/springdocker/SpringDockerApplication.java
@@ -0,0 +1,19 @@
+package juan.springdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringDockerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDockerApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+}
